35fa8109a8284339151d834eda039fddd35ce3f8
avallecam <avallecam@gmail.com>
2019-07-23 10:14:33 -0500

    Update README + Improve heatmap

--- R/cdc_point_heatmap.R
+++ R/cdc_point_heatmap.R
@@ -9,6 +9,10 @@
 #' @param rute_name pending
 #' @param subset_labels pending
 #' @param subset_list pending
+#' @param mp_xlim pending
+#' @param mp_ylim pending
+#' @param mp_dist pending
+#' @param mp_loca pending
 #'
 #' @import dplyr
 #' @import rlang
@@ -45,7 +49,8 @@
 #'@export cdc_point_heat_mark
 #'
 cdc_point_heatmap <- function(data_shape,data_point,rute_name,
-                              subset_labels=F,subset_list) {
+                              subset_labels=F,subset_list,
+                              mp_xlim,mp_ylim,mp_dist,mp_loca) {
 
   heat_colors <- scale_fill_gradientn("Case\ndensity",
                                       colors = c("white", "gold",
@@ -56,6 +61,10 @@ cdc_point_heatmap <- function(data_shape,data_point,rute_name,
 
   caso <- data_point #caso <- piur
   piu4 <- data_shape
+  mp_xlim <- mp_xlim #c(-81.4,-80.2)
+  mp_ylim <- mp_ylim #c(-5.45,-4.4)
+  mp_dist <- mp_dist #10
+  mp_loca <- mp_loca #"bottomright"
 
   name_tit <- piu4 %>% as_tibble() %>%
     count(NM_DEPA) %>% pull(NM_DEPA) %>% as.character()
@@ -74,34 +83,67 @@ cdc_point_heatmap <- function(data_shape,data_point,rute_name,
     select(cod_unico,geometry) %>%
     tidyr::extract(geometry, c('lon', 'lat'), '\\((.*),\\s(.*)\\)', convert = TRUE)
 
+  selected_dist_top <- caso %>%
+    as_tibble() %>%
+    count(ubigeo_res,sort = T) %>%
+    filter(n>2) %>%
+    pull(ubigeo_res)
+
   if (subset_labels==F) {
     point_heat <- ggplot(piu4) +
       stat_density_2d(data = caso_pnt ,
-                      aes(x = lon,y = lat,fill = ..level..),
+                      aes(x = lon,y = lat,fill = ..level..),alpha=0.3,
                       geom = "polygon") +
       heat_colors +
-      geom_sf(alpha=0) +
-      geom_sf(data = caso,alpha=0.5,size=0.5) +
+      geom_sf(alpha=0,colour="gray") +
+      geom_sf(data = caso,alpha=0.5#,size=0.5
+              ) +
+
       geom_sf_text(data = piu4 %>%
                      mutate(NM_DIST=str_replace_all(NM_DIST,"[^[:graph:]]", " ")) %>%
                      filter(CD_DIST %in% subset_list_raw)
                    ,aes(label=NM_DIST),size=2) +
+
+      xlim(mp_xlim) + ylim(mp_ylim) +
+
+      ggsn::scalebar(data = caso %>% filter(prov_res %in% selected_dist_top),
+                     dist = mp_dist,
+                     height = 0.025,st.dist = 0.02,
+                     st.size = 3,dist_unit = "km",
+                     transform = TRUE,
+                     location = mp_loca,
+                     model = "WGS84") +
+
       xlab("Longitud") + ylab("Latitud") +
-      ggtitle(name_tit,subtitle = "Densidad de casos")
+      ggtitle(name_tit#,subtitle = "Densidad de casos"
+              )
   } else{
     point_heat <- ggplot(piu4) +
       stat_density_2d(data = caso_pnt ,
-                      aes(x = lon,y = lat,fill = ..level..),
+                      aes(x = lon,y = lat,fill = ..level..),alpha=0.3,
                       geom = "polygon") +
       heat_colors +
-      geom_sf(alpha=0) +
-      geom_sf(data = caso,alpha=0.5,size=0.5) +
+      geom_sf(alpha=0,colour="gray") +
+      geom_sf(data = caso,alpha=0.5#,size=0.5
+              ) +
       geom_sf_text(data = piu4 %>%
                      mutate(NM_DIST=str_replace_all(NM_DIST,"[^[:graph:]]", " ")) %>%
-                     filter(CD_DIST %in% subset_list)
+                     filter(CD_DIST %in% subset_list_raw)
                    ,aes(label=NM_DIST),size=2) +
+
+      xlim(mp_xlim) + ylim(mp_ylim) +
+
+      ggsn::scalebar(data = caso %>% filter(prov_res %in% selected_dist_top),
+                     dist = mp_dist,
+                     height = 0.025,st.dist = 0.02,
+                     st.size = 3,dist_unit = "km",
+                     transform = TRUE,
+                     location = mp_loca,
+                     model = "WGS84") +
+
       xlab("Longitud") + ylab("Latitud") +
-      ggtitle(name_tit,subtitle = "Densidad de casos")
+      ggtitle(name_tit#,subtitle = "Densidad de casos"
+              )
   }
 
   ggsave(paste0(rute_name,".png"),height = 10,width = 10)
@@ -111,10 +153,6 @@ cdc_point_heatmap <- function(data_shape,data_point,rute_name,
 }
 #' @describeIn cdc_point_heatmap marked heat map
 #' @inheritParams cdc_point_heatmap
-#' @param mp_xlim pending
-#' @param mp_ylim pending
-#' @param mp_dist pending
-#' @param mp_loca pending
 cdc_point_heat_mark <- function(data_shape,data_point,
                                 mp_xlim,mp_ylim,mp_dist,mp_loca) {
   caso <- data_point #piur
--- README.md
+++ README.md
@@ -4,8 +4,24 @@ Paquete en R para automatizar la generación y exploración de reportes, tablas,
 
 ## main functionalities
 
-- `cdc_coropleth` : mapa coropletico estático
-- `cdc_point_heatmap` : mapa con datos puntuales
+- `cdc_mortality`: total de muertes y tasas de mortalidad por combinación de covariables (*dar nombre: cdc_age_adjusted_rates)
+- `cdc_denominator`: generar denominador por combinación de rangos de edad o región administrativa (*bases pendientes)
+- `cdc_mortality_to_wide`: exportar base en formato extendido (wide)
+- `cdc_egresos_summary`: total de egresos por combinación de covariables
+- `cdc_egresos_by_year`: exportar base en formato extendido (wide)
+- `cdc_piramide`: generar pirámide poblacional (*adaptarlo como un geom_piramid para usar con ggplot)
+- `clean_eda_sp`: limpia base consolidada de EDA (vigilancia semanal)
+- `clean_ira_sp`: limpia base consolidada de IRA (vigilancia semanal)
+- `cdc_casos_tiempo`: resume casos por semana de EDA o IRA
+- `cdc_casos_nacional`: resume casos de EDA o IRA en el presente año
+- `cdc_choropleth`: mapa coropletico estático
+- `cdc_choropleth_facet`: mapa coropletico estático segmentado por facets
+- `cdc_morantest`: Ejecuta un test de Moran para poner a prueba la hipotesis de autocorrelación espacial
+- `cdc_rsatscan_clean`: Limpia la salida del rsatscan para la identificación de cluster espacial
+- `cdc_point_heatmap`: mapa de calor de datos puntuales
+- `cdc_point_heat_mark`: mapa de datos puntuales con dos valores a graficar (e.g. casos confirmados y PCR positivos para agente patógeno)
+
+
 
 ## To-Do
 
@@ -13,7 +29,7 @@ Paquete en R para automatizar la generación y exploración de reportes, tablas,
 
 - agregar base de denominadores
 - agregar base de estandarizacion
-- agragar shapefiles
+- agregar shapefiles
 
 * egresos
 - qué pasa con la base de egresos?
