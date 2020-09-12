ae109878dea4a3f3c3eda438b79a64b72420d723
avallecam <avallecam@gmail.com>
2020-09-11 20:38:43 -0500

    add clean cut function

--- R/cdc_misc.R
+++ R/cdc_misc.R
@@ -11,10 +11,12 @@
 #' @return html table
 #'
 #' @export cdc_datatable_html
+#' @export cdc_cut_integer
 #'
 #' @examples
 #'
 #' library(tidyverse)
+#' mtcars %>% as_tibble() %>% cdc_cut_integer(qsec,3) %>% count(qsec_cut)
 #' iris %>% group_by(Species) %>% summarise_all(.funs = mean) %>% cdc_datatable_html()
 #'
 
@@ -24,4 +26,21 @@ cdc_datatable_html <- function(data) {
     DT::datatable(
       options = list(
         language = list(url = '//cdn.datatables.net/plug-ins/1.10.11/i18n/Spanish.json')))
+}
+
+#' @describeIn cdc_datatable_html priorización con dos covariables
+#' @inheritParams cdc_datatable_html
+#' @param variable variable a usar
+#' @param number_cuts numbers of cuts
+
+cdc_cut_integer <- function(data,variable,number_cuts=4,end_string="_cut") {
+  c_var <- enquo(variable)
+  c_var_name_01 <- c_var %>% rlang::as_name() %>% str_c(.,{{end_string}})
+  data %>%
+    mutate(!!c_var_name_01 := cut({{variable}},
+                                  breaks = quantile(data %>% pull({{variable}}), 0:{{number_cuts}}/{{number_cuts}},na.rm = T),
+                                  # breaks = 4,
+                                  # breaks = c(0,25,75,125,175,225,300)
+                                  include.lowest = T,
+                                  dig.lab = 2))
 }
